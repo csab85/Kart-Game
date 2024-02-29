43a1a1a3eac18785d9dd8d783eb9d38ccb52b49d
csab85 <csantanna998@gmail.com>
2024-02-29 20:50:30 -0300

    New Pod
    Added the new pod model and its movement code

---
+++ Assets/Scripts/Pod/PodMovement.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.EditorTools;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class PodMovement: MonoBehaviour
+{
+    //IMPORTS
+    //========================
+    #region
+
+
+
+    #endregion
+    //========================
+
+
+    //STATS AND VALUES
+    //========================
+    #region
+
+    [Header ("Movement Config")]
+
+    [SerializeField] float acceleration;
+    [SerializeField] float reverseAcceleration;
+    [SerializeField] float dragGrowth;
+    [SerializeField] [Tooltip ("How fast the pod turns its body")] float angularAcceleration;
+    [SerializeField] float angularDragGrowth;
+    
+
+    //components
+    Rigidbody rb;
+
+    #endregion
+    //========================
+
+
+    //FUNCTIONS
+    //========================
+    #region
+
+    /// <summary>
+    /// Moves the pod forward or backwards
+    /// </summary>
+    /// <param name="direction">If positive the pod goes forward, if negative goes backwards</param>
+    public void Move(float direction)
+    {
+        if(direction > 0)
+        {
+            rb.AddForce(transform.forward * acceleration);
+        }
+
+        if(direction < 0)
+        {
+            rb.AddForce(transform.forward * -reverseAcceleration);
+        }
+
+        rb.drag = rb.velocity.magnitude * dragGrowth;
+    }
+
+    /// <summary>
+    /// Makes the pod turn on its y axis
+    /// </summary>
+    /// <param name="side">If positive turns to the right, if negative to the left</param>
+    public void Turn(float side)
+    {
+        if (side > 0)
+        {
+            rb.AddTorque(new Vector3(0, angularAcceleration, 0));
+        }
+
+        if (side < 0)
+        {
+            rb.AddTorque(new Vector3(0, -angularAcceleration, 0));
+        }
+
+        rb.angularDrag = rb.angularVelocity.magnitude * angularDragGrowth;
+    }
+
+    #endregion
+    //========================
+
+
+    //RUNNING
+    //========================
+    #region
+
+    //Start
+    void Start()
+    {
+        //get components
+        rb = GetComponent<Rigidbody>();
+    }
+
+    //Update
+    void FixedUpdate()
+    {
+
+    }
+
+    #endregion
+    //========================
+}
